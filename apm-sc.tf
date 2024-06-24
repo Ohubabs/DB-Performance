2f9a5dbade75d1f0bbf31ca706f5e0cdfc09db1d
ohubabs <obakinolugbade@gmail.com>
2024-06-24 13:39:29 -0700

    storageclasses to enable dynmanic provisioning for all monitoring statefulset deployments

---
+++ apm-sc.tf
@@ -0,0 +1,66 @@
+resource "kubernetes_storage_class" "prom-sc" {
+  metadata {
+    name = "prom-sc"
+  }
+  storage_provisioner = "ebs.csi.aws.com"
+  volume_binding_mode = "Immediate"
+  parameters = {
+    type = "gp3"
+    iops = 3000
+    encrypted = "true"
+  }
+}
+
+resource "kubernetes_storage_class" "prom-sc2" { #storage for alert manager
+  metadata {
+    name = "prom-sc2"
+  }
+  storage_provisioner = "ebs.csi.aws.com"
+  volume_binding_mode = "Immediate"
+  parameters = {
+    type = "gp3"
+    iops = 3000
+    #encrypted = "true"
+  }
+}
+
+resource "kubernetes_storage_class" "graf-sc" {
+  metadata {
+    name = "graf-sc"
+  }
+  storage_provisioner = "ebs.csi.aws.com"
+  volume_binding_mode = "Immediate"
+  parameters = {
+    type = "gp3"
+    iops = 3000
+    #encrypted = "true"
+  }
+}
+
+/*resource "kubernetes_storage_class" "elasticsearch-sc" {
+  metadata {
+    name = "elasticsearch-sc"
+  }
+  storage_provisioner = "ebs.csi.aws.com"
+  reclaim_policy      = "Retain"
+  volume_binding_mode = "Immediate"
+  parameters = {
+    type = "gp3"
+    iops = 3000
+    encrypted = "true"
+  }
+}
+
+resource "kubernetes_storage_class" "logstash-sc" {
+  metadata {
+    name = "logstash-sc"
+  }
+  storage_provisioner = "ebs.csi.aws.com"
+  reclaim_policy      = "Retain"
+  volume_binding_mode = "Immediate"
+  parameters = {
+    type = "gp3"
+    iops = 3000
+    encrypted = "true"
+  }
+}*/
