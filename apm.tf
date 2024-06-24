63c9da7c026cfa16853ca164d8e63deb3d997ea5
ohubabs <obakinolugbade@gmail.com>
2024-06-24 13:40:34 -0700

    Deploy Prometheus, Grafana, Elasticsearch, Logstash, and Kibana

---
+++ apm.tf
@@ -0,0 +1,60 @@
+resource "helm_release" "prometheus" {
+  name       = "prometheus"
+  create_namespace = true
+  namespace  = "apm"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "prometheus"
+
+  values = [
+    "${file("prom-values.yml")}"
+  ]
+}
+
+#Ref: https://developer.hashicorp.com/terraform/tutorials/kubernetes/helm-provider
+#https://github.com/dmeiners88/sonarqube-prometheus-exporter
+
+resource "helm_release" "grafana" {
+  name       = "grafana"
+  create_namespace = true
+  namespace  = "apm"
+  repository = "https://grafana.github.io/helm-charts"
+  chart      = "grafana"
+
+  values = [
+    "${file("graf-values.yml")}"
+  ]
+}
+
+/*resource "helm_release" "elasticsearch" {
+  name       = "elasticsearch"
+  namespace = "monitor"
+  repository = "https://helm.elastic.co"
+  chart      = "elasticsearch"
+
+  values = [
+    "${file("elasticsearch.yml")}"
+  ]
+}
+
+resource "helm_release" "kibana-14" {
+  name       = "kibana-14"
+  namespace = "monitor"
+  repository = "https://helm.elastic.co"
+  chart      = "kibana"
+
+  values = [
+    "${file("kibana.yml")}"
+  ]
+}
+
+resource "helm_release" "logstash" {
+  name       = "logstash"
+  namespace = "monitor"
+  repository = "https://helm.elastic.co"
+  chart      = "logstash"
+
+  values = [
+    "${file("logstash.yml")}"
+  ]
+}*/
+
