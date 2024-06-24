1b35eaf945bc7a4ae835dfcb3e95807d9b988e51
Babs <68171102+Ohubabs@users.noreply.github.com>
2024-06-24 16:02:56 -0700

    Update apm.tf

--- apm.tf
+++ apm.tf
@@ -1,7 +1,7 @@
 resource "helm_release" "prometheus" {
   name       = "prometheus"
   create_namespace = true
-  namespace  = "apm"
+  namespace  = "monitor"
   repository = "https://prometheus-community.github.io/helm-charts"
   chart      = "prometheus"
 
@@ -16,7 +16,7 @@ resource "helm_release" "prometheus" {
 resource "helm_release" "grafana" {
   name       = "grafana"
   create_namespace = true
-  namespace  = "apm"
+  namespace  = "monitor"
   repository = "https://grafana.github.io/helm-charts"
   chart      = "grafana"
 
