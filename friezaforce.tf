4254a11f634b902c08bb143b2732f060cd14ca96
Babs <68171102+Ohubabs@users.noreply.github.com>
2024-10-03 08:27:08 -0700

    Create friezaforce.tf

---
+++ friezaforce.tf
@@ -0,0 +1,28 @@
+
+resource "kubernetes_secret_v1" "graf-auth" {
+  metadata {
+    name = "vegeta"
+    namespace = "monitor"
+  }
+
+  data = {
+    garlickgun = var.garlickgun
+    finalflash = var.finalflash
+  }
+
+  type = "Opaque"
+}
+
+resource "kubernetes_secret_v1" "elastic-auth" {
+  metadata {
+    name = "food-1"
+    namespace = "apm"
+  }
+
+  data = {
+    username = var.frieza
+    password = var.deathbeam
+  }
+
+  type = "Opaque"
+}
