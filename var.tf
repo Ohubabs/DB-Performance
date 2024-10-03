89f69c09ff1c364d3b4f690f4e07862522384a2d
Babs <68171102+Ohubabs@users.noreply.github.com>
2024-10-03 08:25:22 -0700

    Create var.tf

---
+++ var.tf
@@ -0,0 +1,19 @@
+variable "garlickgun" {
+    description = "Username for Grafana Dashboard"
+    default = "" 
+}
+
+variable "finalflash" {
+    description = "Password for Grafana Dashboard"
+    default = "" 
+}
+
+variable "frieza" {
+    description = "Username for Elastic/Kibana Dashboard"
+    default = "elastic" 
+}
+
+variable "deathbeam" {
+    description = "Password for Elastic/Kibana Dashboard"
+    default = "" 
+}
