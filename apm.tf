5efdb8b14de8d31c30f28aa102ad7bf373fed85f
Babs <68171102+Ohubabs@users.noreply.github.com>
2024-10-08 12:30:57 -0700

    Update apm.tf

--- apm.tf
+++ apm.tf
@@ -25,36 +25,3 @@ resource "helm_release" "grafana" {
   ]
 }
 
-/*resource "helm_release" "elasticsearch" {
-  name       = "elasticsearch"
-  namespace = "monitor"
-  repository = "https://helm.elastic.co"
-  chart      = "elasticsearch"
-
-  values = [
-    "${file("elasticsearch.yml")}"
-  ]
-}
-
-resource "helm_release" "kibana" {
-  name       = "kibana"
-  namespace = "monitor"
-  repository = "https://helm.elastic.co"
-  chart      = "kibana"
-
-  values = [
-    "${file("kibana.yml")}"
-  ]
-}
-
-resource "helm_release" "logstash" {
-  name       = "logstash"
-  namespace = "monitor"
-  repository = "https://helm.elastic.co"
-  chart      = "logstash"
-
-  values = [
-    "${file("logstash.yml")}"
-  ]
-}*/
-
