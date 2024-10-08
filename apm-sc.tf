b7016958dc1944fa6ee9bd595f538eb691c5e677
Babs <68171102+Ohubabs@users.noreply.github.com>
2024-10-08 12:30:33 -0700

    Update apm-sc.tf

--- apm-sc.tf
+++ apm-sc.tf
@@ -37,30 +37,3 @@ resource "kubernetes_storage_class" "graf-sc" {
   }
 }
 
-/*resource "kubernetes_storage_class" "elasticsearch-sc" {
-  metadata {
-    name = "elasticsearch-sc"
-  }
-  storage_provisioner = "ebs.csi.aws.com"
-  reclaim_policy      = "Retain"
-  volume_binding_mode = "Immediate"
-  parameters = {
-    type = "gp3"
-    iops = 3000
-    encrypted = "true"
-  }
-}
-
-resource "kubernetes_storage_class" "logstash-sc" {
-  metadata {
-    name = "logstash-sc"
-  }
-  storage_provisioner = "ebs.csi.aws.com"
-  reclaim_policy      = "Retain"
-  volume_binding_mode = "Immediate"
-  parameters = {
-    type = "gp3"
-    iops = 3000
-    encrypted = "true"
-  }
-}*/
