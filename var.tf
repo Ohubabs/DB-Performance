b3c8940cac5c8d2e87fd4e985fa3bb05431aa4a9
Babs <68171102+Ohubabs@users.noreply.github.com>
2024-10-08 12:28:46 -0700

    Update var.tf

--- var.tf
+++ var.tf
@@ -8,12 +8,4 @@ variable "finalflash" {
     default = "" 
 }
 
-variable "frieza" {
-    description = "Username for Elastic/Kibana Dashboard"
-    default = "elastic" 
-}
 
-variable "deathbeam" {
-    description = "Password for Elastic/Kibana Dashboard"
-    default = "" 
-}
